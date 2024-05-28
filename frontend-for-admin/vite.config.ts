896bd104a067a6d4b82241d286f095eb3d4b10e6
Sadullo <sadulloburiyev1992@gmail.com>
2024-05-28 22:22:35 +0500

    feat: enable to vite config

--- frontend-for-admin/vite.config.ts
+++ frontend-for-admin/vite.config.ts
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [react()],
-  preview: {
-    port: 3000,
-    strictPort: true,
-  },
-  server: {
-    port: 3000,
-    strictPort: true,
-    host: true,
-    origin: 'http://0.0.0.0:3000',
-  },
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    base: '/',
+    plugins: [react()],
+    preview: {
+      port: +env.PORT_FE,
+    },
+    server: {
+      port: +env.PORT_FE,
+      host: true,
+      origin: `http://0.0.0.0:${env.PORT_FE}`,
+    },
+  };
 });
