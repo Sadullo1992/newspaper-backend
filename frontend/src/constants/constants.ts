a44a9192df5f2f5aec911e00ca6d67c25240fadb
Sadullo <sadulloburiyev1992@gmail.com>
2024-06-06 01:21:24 +0500

    feat: config vite env and server proxy

--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = 'https://uzunpro.uz/api/v1';
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL || '/api';
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,13 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    base: '/',
-    plugins: [react()],
-    preview: {
-      port: +env.PORT_FE,
-    },
-    server: {
-      port: +env.PORT_FE,
-      host: true,
-      watch: {
-        usePolling: true,
+export default defineConfig({
+  server: {
+    proxy: {
+      '^/api/.*': {
+        target: 'http://localhost:4200',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
-      origin: `http://0.0.0.0:${env.PORT_FE}`,
     },
-  };
+  },
 });
