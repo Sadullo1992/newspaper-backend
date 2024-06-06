63056f0d6636aae65c7b1a3f3fb25172cfef88dd
Sadullo <sadulloburiyev1992@gmail.com>
2024-06-06 11:27:32 +0500

    feat: add nginx and update dockerfile

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 100,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
   server: {
     proxy: {
       '^/api/.*': {
