2d3ab1b88f498849d6366e2cd8045800595f46d5
Sadullo <sadulloburiyev1992@gmail.com>
2024-06-11 19:44:28 +0500

    feat: add .env examples

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   server: {
     proxy: {
       '^/api/.*': {
-        target: 'http://localhost:4200',
+        target: 'http://localhost:4000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
