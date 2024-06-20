c09a971e2e3588fc48da450fc3dbb81de175ac0d
Sadullo <sadulloburiyev1992@gmail.com>
2024-06-20 18:30:51 +0500

    fix: update category service

--- backend/src/admin/category/category.service.ts
+++ backend/src/admin/category/category.service.ts
@@ -1,6 +1,32 @@
 import { Injectable } from '@nestjs/common';
-import { DatabaseService } from 'src/database/database.service';
-import { Category } from './entities/category.entity';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Injectable()
-export class CategoryService extends DatabaseService<Category> {}
+export class CategoryService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createCategoryDto: CreateCategoryDto) {
+    return this.prisma.category.create({ data: createCategoryDto });
+  }
+
+  async findAll() {
+    return this.prisma.category.findMany();
+  }
+
+  async findOne(id: string) {
+    return this.prisma.category.findUnique({ where: { id } });
+  }
+
+  async update(id: string, updateCategoryDto: UpdateCategoryDto) {
+    return this.prisma.category.update({
+      where: { id },
+      data: updateCategoryDto,
+    });
+  }
+
+  async remove(id: string) {
+    return this.prisma.category.delete({ where: { id } });
+  }
+}
