dcf7d92db300507dbfba0bf1b2b403d0ac0c663e
Sadullo <sadulloburiyev1992@gmail.com>
2024-08-15 22:26:56 +0500

    fix: remove req body from logs

--- backend/src/logger/logger.interceptor.ts
+++ backend/src/logger/logger.interceptor.ts
@@ -12,14 +12,12 @@ export class LoggerInterceptor implements NestInterceptor {
   constructor(private logService: LogService) {}
   intercept(context: ExecutionContext, next: CallHandler): Observable<void> {
     const [req, res] = context.getArgs();
-    const { method, url, query, body } = req;
+    const { method, url, query } = req;
     const { statusCode } = res;
     const now = Date.now();
 
     const message = ` {${url}, ${method}, query: ${JSON.stringify(
       query,
-    )}, body: ${JSON.stringify(
-      body,
     )} } statusCode: ${statusCode}, responseTime: [${Date.now() - now}ms]`;
 
     return next
