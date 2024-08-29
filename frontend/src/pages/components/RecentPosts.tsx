a900b90c4e77ad32749175899938a76ecaf6a6c4
Sadullo <sadulloburiyev1992@gmail.com>
2024-08-30 00:18:24 +0500

    feat: integrate posts

--- frontend/src/pages/components/RecentPosts.tsx
+++ frontend/src/pages/components/RecentPosts.tsx
@@ -1,57 +1,54 @@
+import { CalendarOutlined, EyeOutlined } from '@ant-design/icons';
 import { Flex, Image, List, Space, Typography } from 'antd';
-import { EyeOutlined, CalendarOutlined } from '@ant-design/icons';
 import React from 'react';
+import { usePostsQuery } from '../../queries/posts';
+import { dateFormatter } from '../../utils/dateFormatter';
 
 export const RecentPosts = () => {
-    const data = [
-      {
-        title:
-          'Ant Design Title 1 a design language for background applications a design language for background applications',
-      },
-      {
-        title: 'Ant Design Title 2',
-      },
-      {
-        title: 'Ant Design Title 3',
-      },
-      {
-        title: 'Ant Design Title 4',
-      },
-    ];
-    return (
-      <List
-        itemLayout="vertical"
-        dataSource={data}
-        renderItem={(item) => (
-          <List.Item>
-            <List.Item.Meta
-              avatar={
-                <Image
-                  style={{ width: 100, height: 'auto', aspectRatio: 16 / 9, borderRadius: '4px' }}
-                  src={`https://uzunpro.uz/api/media/images/photo_2024-08-21_15-49-33.jpg`}
+  const { data, isLoading } = usePostsQuery({
+    page: 1,
+    perPage: 10,
+  });
+
+  return (
+    <List
+      itemLayout="vertical"
+      dataSource={data?.data}
+      loading={isLoading}
+      renderItem={(item) => (
+        <List.Item>
+          <List.Item.Meta
+            avatar={
+              <Image
+                style={{ width: 100, height: 'auto', aspectRatio: 16 / 9, borderRadius: '4px' }}
+                src={`https://uzunpro.uz/api/media/images/${item?.images[0].imagename}`}
+              />
+            }
+            title={
+              <Typography.Link ellipsis href={`/admin/post/${item.id}/edit`}>
+                {item.title}
+              </Typography.Link>
+            }
+            description={
+              <Flex gap={16}>
+                <IconText
+                  icon={EyeOutlined}
+                  text={`${item.views} Views`}
+                  key="list-vertical-eye-o"
                 />
-              }
-              title={
-                <Typography.Link ellipsis href="https://ant.design">
-                  {item.title}
-                </Typography.Link>
-              }
-              description={
-                <Flex gap={16}>
-                  <IconText icon={EyeOutlined} text="156 Views" key="list-vertical-eye-o" />
-                  <IconText
-                    icon={CalendarOutlined}
-                    text="29.08.2024"
-                    key="list-vertical-like-o"
-                  />
-                </Flex>
-              }
-            />
-          </List.Item>
-        )}
-      />
-    );
-}
+                <IconText
+                  icon={CalendarOutlined}
+                  text={dateFormatter(item.createdAt)}
+                  key="list-vertical-like-o"
+                />
+              </Flex>
+            }
+          />
+        </List.Item>
+      )}
+    />
+  );
+};
 
 export const IconText = ({ icon, text }: { icon: React.FC; text: string }) => (
   <Space>
