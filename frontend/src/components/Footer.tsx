242bd4ebaf7d75fce8862d8d44437df971dd814b
Sadullo <sadulloburiyev1992@gmail.com>
2024-09-19 17:25:01 +0500

    fix: fix footer content

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Layout } from 'antd';
 export const Footer = () => {
   return (
     <Layout.Footer style={{ textAlign: 'center' }}>
-      Bobotogtongi ©{new Date().getFullYear()} Created by Uzunsoft
+      Bobotogtongi News ©{new Date().getFullYear()}. All Rights Reserved.
     </Layout.Footer>
   );
 };
